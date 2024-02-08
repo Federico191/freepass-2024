8878ede108a829e6a97bee2c2e9bb7794a333e7c
Federico191 <federicoberto708@gmail.com>
2024-02-08 12:01:18 +0700

    remove isVoted and VotedElectionNumber properties

--- internal/entity/account_entity.go
+++ internal/entity/account_entity.go
@@ -6,15 +6,13 @@ import (
 )
 
 type Account struct {
-	ID                  uint      `gorm:"primaryKey;autoIncrement"`
-	Avatar              *string   `gorm:"size:255"`
-	Username            string    `gorm:"size:20;not null"`
-	User                User      `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	BirthDate           time.Time `gorm:"type:date;not null"`
-	IsVoted             *bool     `gorm:"default:false"`
-	VotedElectionNumber *uint
-	Candidate           []Candidate `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	CreatedAt           time.Time   `gorm:"autoCreateTime:milli"`
-	UpdatedAt           time.Time   `gorm:"autoCreateTime:milli;autoUpdateTime:milli"`
-	DeletedAt           gorm.DeletedAt
+	ID        uint        `gorm:"primaryKey;autoIncrement"`
+	Avatar    *string     `gorm:"size:255"`
+	Username  string      `gorm:"size:20;not null"`
+	User      User        `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	BirthDate time.Time   `gorm:"type:date;not null"`
+	Candidate []Candidate `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	CreatedAt time.Time   `gorm:"autoCreateTime:milli"`
+	UpdatedAt time.Time   `gorm:"autoCreateTime:milli;autoUpdateTime:milli"`
+	DeletedAt gorm.DeletedAt
 }
